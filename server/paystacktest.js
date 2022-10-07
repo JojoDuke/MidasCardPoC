f995e2a2bc2afe1d211d39267781a8b9b540c454
JojoDuke <jojoamankwa@gmail.com>
2022-10-07 11:37:08 +0000

    theory

--- server/paystacktest.js
+++ server/paystacktest.js
@@ -3,7 +3,7 @@ const https = require('https');
 const APIKEY = 'sk_live_95295f9cf3433e9918b9387b8b93a3acdee920c6';
 
 const params = JSON.stringify({
-    "amount": 100,
+    "amount": 200,
     "email": "dukeopoku@gmail.com",
     "currency": "GHS",
     "mobile_money": { 
