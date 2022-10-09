2a2a4729fc849985b13a73677a6eba25c8c88834
JojoDuke <jojoamankwa@gmail.com>
2022-10-09 13:15:49 +0000

    added axios

--- server/paystacktest.js
+++ server/paystacktest.js
@@ -1,9 +1,19 @@
 const https = require('https');
+const express = require('express');
+const cors = require('cors');
+const { default: axios } = require('axios');
+const app = express();
+
+const PORT = 4001;
+
+app.use(express.json());
+app.use(cors());
+
 
 const APIKEY = 'sk_live_95295f9cf3433e9918b9387b8b93a3acdee920c6';
 
 const params = JSON.stringify({
-    "amount": 200,
+    "amount": 100,
     "email": "dukeopoku@gmail.com",
     "currency": "GHS",
     "mobile_money": { 
@@ -16,26 +26,26 @@ const params = JSON.stringify({
     hostname: 'api.paystack.co',
     port: 443,
     path: '/charge',
+    url: "https://api.paystack.co/charge",
     method: 'POST',
     headers: {
       Authorization: `Bearer ${ APIKEY }`,
       'Content-Type': 'application/json'
-    }
+    },
+    data: params
   }
 
-  const req = https.request(options, res => {
-    let data = ''
   
-    res.on('data', (chunk) => {
-      data += chunk
-    });
-  
-    res.on('end', () => {
-      console.log(JSON.parse(data))
-    })
-  }).on('error', error => {
-    console.error(error)
+  app.get("/perform", async (req, res) => {
+    axios(options)
+      .then(response => {
+        res.send((response.data));
+      })
+      .catch(error => {
+        res.send(error);
+      })
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   })
-  
-  req.write(params)
-  req.end()
